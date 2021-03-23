7b00d813724db5b143c93e16d69057fcff422e1b
ji sun lee <ellijs@hotmail.com>
2021-03-23 21:49:04 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,3 +12,15 @@ function whileLoop(number) {
   }
   return "done";
 }
+
+let i = 0;
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(num) {
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable() < num)
+}
